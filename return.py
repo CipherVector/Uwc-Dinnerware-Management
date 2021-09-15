d7ce788fcc2ed5a5be4a8cbb688a8e6d9571be7d
xss <58095671+CipherVector@users.noreply.github.com>
2021-09-15 07:39:14 -0700

    Added new fixes

--- return.py
+++ return.py
@@ -3,9 +3,9 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 api = FirebaseApi(config['creds_file'], config['db_url'], config['location_id'])
-
+Detecttype = "QRCODE"
 
 while True:
-  ItemID = cameraQR()
+  ItemID = camera(Detecttype)
   api.returnItem(ItemID)
   
